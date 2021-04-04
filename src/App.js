f0785bcf7a839e132ea2e36f83ac0847ccae30c2
kirillchern <kiril.chwork@gmail.com>
2021-04-04 23:33:38 +0300

    stopwatch react rxjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import { interval, Subject, fromEvent } from "rxjs";
+import {
+  startWith,
+  takeWhile,
+  buffer,
+  filter,
+  debounceTime,
+} from "rxjs/operators";
+import "./App.css";
+
+let observable$ = interval(10).pipe(startWith(0));
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+  const [resetOn, setResetOn] = useState(true);
+
+  const action$ = new Subject();
+
+  action$.subscribe((v) => {
+    switch (v) {
+      case "start":
+        setTimerOn((state) => !state);
+        break;
+      case "stop":
+        setTimerOn((state) => !state);
+        setTimer(0);
+        break;
+      case "wait":
+        setTimerOn(false);
+        break;
+      case "resetOn":
+        setResetOn(false);
+        setTimerOn(true);
+        setTimer(0);
+        break;
+      case "resetOff":
+        setResetOn(true);
+        setTimerOn(true);
+        setTimer(0);
+        break;
+    }
+  });
+
+  useEffect(() => {
+    let sub = observable$
+      .pipe(takeWhile(() => timerOn))
+      .subscribe((value) => setTimer(timer + value * 10));
+
+    return () => sub.unsubscribe();
+  }, [timerOn, resetOn]);
+
+  const waitBtnRef = useRef(null);
+
+  const clicks$ = fromEvent(document.getElementById("root"), "click");
+  clicks$
+    .pipe(
+      buffer(clicks$.pipe(debounceTime(300))),
+      filter((clickArray) => clickArray.length === 2)
+    )
+    .subscribe(() => action$.next("wait"));
+
+  const startBtn = (
+    <button className="start-btn btn" onClick={() => action$.next("start")}>
+      Start
+    </button>
+  );
+  const stopBtn = (
+    <button className="stop-btn btn" onClick={() => action$.next("stop")}>
+      Stop
+    </button>
+  );
+  const waitBtn = (
+    <button id="root" className="wait-btn btn" ref={waitBtnRef}>
+      Wait
+    </button>
+  );
+  const resetBtn = (
+    <button
+      className="reset-btn btn"
+      onClick={() =>
+        resetOn ? action$.next("resetOn") : action$.next("resetOff")
+      }
+    >
+      Reset
+    </button>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="timer">
+        <div className="timer-items">
+          <span>{("0" + Math.floor((timer / 60000) % 60)).slice(-2)}:</span>
+          <span>{("0" + Math.floor((timer / 1000) % 60)).slice(-2)}:</span>
+          <span>{("0" + ((timer / 10) % 100)).slice(-2)}</span>
+        </div>
+      </div>
+      <div className="btns">
+        {!timerOn && startBtn}
+        {timerOn && stopBtn}
+        {(timerOn || !!timer) && waitBtn}
+        {(timerOn || !!timer) && resetBtn}
+      </div>
     </div>
   );
 }
